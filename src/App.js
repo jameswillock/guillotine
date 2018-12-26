bf7aa2d1ded8ab1314bde17b6410868ffe3e5d46
James Willock <james.willock@gmail.com>
2018-12-26 12:42:51 +0000

    More tests

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Game from './components/Game';
 import './App.css';
 
-
 class App extends Component {
   render() {
     return <Game />;
---
+++ src/components/Guesses.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Guesses from './Guesses';
+import { render, cleanup, fireEvent, getByText } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+afterEach(cleanup);
+
+test('It renders Keycaps for each character', () => {
+  const { getByText } = render(
+    <Guesses guessClickHandler={() => true}
+      resetClickHandler={() => true}
+      guessed={new Set()} />
+  );
+
+  const letters = [
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U',
+    'I', 'O', 'P', 'A', 'S', 'D', 'F',
+    'G', 'H', 'J', 'K', 'L', 'Z', 'X',
+    'C', 'V', 'B', 'N', 'M'
+  ];
+  
+  letters.forEach(letter => expect(getByText(letter)).toBeInTheDocument());
+
+  expect(getByText('Reset')).toBeInTheDocument();
+});
