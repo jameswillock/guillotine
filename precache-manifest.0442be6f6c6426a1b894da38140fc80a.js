d678f4482da63e01819127dfb5184e5c00a7d019
James Willock <james.willock@gmail.com>
2019-01-10 15:42:39 +0000

    Updates

---
+++ precache-manifest.0442be6f6c6426a1b894da38140fc80a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed4492bae3134966757f",
+    "url": "/guillotine/static/css/main.14043580.chunk.css"
+  },
+  {
+    "revision": "ed4492bae3134966757f",
+    "url": "/guillotine/static/js/main.ed4492ba.chunk.js"
+  },
+  {
+    "revision": "d7dc94970a9cb1a50d42",
+    "url": "/guillotine/static/js/1.d7dc9497.chunk.js"
+  },
+  {
+    "revision": "b274b6ac34dfb77d1e45",
+    "url": "/guillotine/static/js/runtime~main.b274b6ac.js"
+  },
+  {
+    "revision": "85558000bc0c2f11bcff8444d329634c",
+    "url": "/guillotine/index.html"
+  }
+];
